626f0fb55a26b006fbff8da1ac96b984ebdbe06a
hwld <0701hw@gmail.com>
2021-08-12 17:13:32 +0900

    6

--- src/App.js
+++ src/App.js
@@ -10,13 +10,20 @@ import './App.css';
 export const App = () => {
   const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
 
+  const handleChangeDogImage = async () => {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await res.json();
+
+    setDogUrl(data.message);
+  }
+
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです</p>
       <img  src={dogUrl}></img>
       <div>
-        <button onClick={()=>setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg ")}>
+        <button onClick={handleChangeDogImage}>
           変更
         </button>
       </div>

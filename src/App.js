fb3cd34addd1d56f76990ae5856ea417101190ef
hwld <0701hw@gmail.com>
2021-08-12 17:07:28 +0900

    4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 // DO NOT DELETE
 
-import * as React from 'react'
-import './App.css'
+import * as React from 'react';
+import './App.css';
 
 /**
  * 
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = React.useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
+
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです</p>
-      <img  src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg"></img>
+      <img  src={dogUrl}></img>
     </div>
   )
 }
